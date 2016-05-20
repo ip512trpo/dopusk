293f1fc5de546604415f2f0692af12e6926b32c9
Alexey Nikulin <alex-nsk-1996@mail.ru>
2016-05-20 17:51:52 +0600

    test added

--- test.cpp
+++ test.cpp
@@ -6,17 +6,30 @@
 
 
 int main()
-{
-	float i, j, k;
+{	
+	float i, j, k, x1, x2;
 	float min = 1, max = 50;
 	FILE* discOut = fopen("disc.txt", "w");
 	for(i = min; i<max; i++){
 		for(j = min; j<max; j++){
 			for(k = min; k<max; k++){
+				
 		fprintf(discOut, "a = %.0f, b = %0.f, c = %0.f, D = %.3f", i, j, k, disc(i, j, k));
-		if(disc(i, j, k) < 0)
-			fprintf(discOut, "  Discriminant less than zero");
+		
+				if(disc(i, j, k) < 0)
+		{ 
+			fprintf(discOut, "  Discriminant less than zero"); 
+		}
+		
+		else
+		{
+		x1 = (-j-sqrt(disc(i, j, k)))/(2*i);
+		x2 = (-j+sqrt(disc(i, j, k)))/(2*i);
+		if(i*(x1*x1)+j*x1+k > i*(x2*x2)+j*x2+k -0.0001 || i*(x1*x1)+j*x1+k < i*(x2*x2)+j*x2+k + 0.0001  )
+			fprintf(discOut, " %0.f*(%0.f^2) + %0.f*(%0.f) + %0.f = %0.f*%0.f^2 + %0.f*%0.f + k = 0", i, x1, j, x1, k, i, x2, j, x2, k);
+		}
 		fprintf(discOut, "\n");
+			
 			}
 		}
 	}
